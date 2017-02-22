50a66f1c901c0988d5188e37c8c460ce2f5d159e
Jenny Liang <Jenny@D-10-157-125-161.dhcp4.washington.edu>
2017-02-22 15:33:17 -0800

    Factored code

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@
 
 var overlay = document.querySelector(".overlay");
 var trapezoid = document.querySelector(".trapezoid");
-overlay.addEventListener("click", function hide() {
-    trapezoid.classList.toggle("hide");
-    overlay.classList.toggle("hide");
-});
 
-var readMore = document.querySelectorAll(".mdl-button")
-for (var i = 0; i < readMore.length; i++) {
-    var button = readMore[i];
-    button.addEventListener("click", function show() {
+function addHide(object) {
+    object.addEventListener("click", function hide() {
         trapezoid.classList.toggle("hide");
         overlay.classList.toggle("hide");
     });
+}
+
+addHide(overlay);
+addHide(trapezoid);
+var readMore = document.querySelectorAll(".mdl-button")
+for (var i = 0; i < readMore.length; i++) {
+    var button = readMore[i];
+    addHide(button);
 }
