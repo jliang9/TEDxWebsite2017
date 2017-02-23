e15d19d227ac283e14e3f3fe748724b4a023b360
Jenny Liang <Jenny@D-10-157-125-161.dhcp4.washington.edu>
2017-02-22 16:20:09 -0800

    Change which speakers appear based off of what year is selected

--- js/app.js
+++ js/app.js
@@ -17,3 +17,36 @@ for (var i = 0; i < readMore.length; i++) {
     var button = readMore[i];
     addHide(button);
 }
+
+/* var years = [document.querySelector("#2017"), document.querySelector("#2016"), document.querySelector("#2015"), document.querySelector("#2014")];
+var allSpeakers = document.querySelectorAll(".mdl-card")
+for (var j = 0; j < years.length; j++) {
+    var year = years[i];
+    var cssVar = 2017 - i;
+    year.addEventListener("click", function showSpeakers() {
+        makeInactive(years, cssVar);
+        showSpeakersHelper("speaker-" + cssVar);
+    });
+}
+
+function makeInactive(array, activeName) {
+    for (var i = 0; i < array.length; i++) {
+        var item = array[i];
+        if (item.id == activeName) {
+            item.classList.add("active");
+        } else {
+            item.classList.remove("active");
+        }
+    }
+}
+
+function showSpeakersHelper(className) {
+    for (int i = 0; i < allSpeakers.length; i++) {
+        var speaker = allSpeakers[i];
+        if (speaker.classList.contains(className)) {
+            speaker.classList.remove("hide");
+        } else {
+            speaker.classList.add("hide");
+        }
+    }
+} */
